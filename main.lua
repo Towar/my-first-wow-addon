4c919ea62bdf5acb038ebe711b076e2b04c4f44b
Piotr Myszczyński <pmyszczynski@gmail.com>
2017-12-23 15:35:23 +0100

    addon init with hello world message to user

---
+++ main.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: piotrmyszczynski
+-- Date: 23/12/2017
+-- Time: 15:33
+-- To change this template use File | Settings | File Templates.
+--
+
+message('HelloWorld')
